5f7e6f09abc4ddfd744951d93ac9636bb94a2dd9
HatemRamadan <hatemram96@gmail.com>
2021-01-28 23:51:25 +0200

    first commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,60 +1,94 @@
-import React from "react";
-import myImage from "../img/myImage.png";
+import React from 'react';
+import myImage from '../img/myImage.png';
 
 class About extends React.Component {
   constructor() {
     super();
     this.state = {
       skills: [
-        { id: "HTML5_skill", content: "HTML5", porcentage: "80%", value: "80" },
-        { id: "CSS3_skill", content: "CSS3", porcentage: "75%", value: "75" },
         {
-          id: "JavaScript_skill",
-          content: "JavaScript",
-          porcentage: "90%",
-          value: "90"
-        },
-        { id: "PHP_skill", content: "PHP", porcentage: "70%", value: "70" },
-        {
-          id: "ReactJS_skill",
-          content: "ReactJS",
-          porcentage: "80%",
-          value: "80"
+          id: 'ReactJS_skill',
+          content: 'ReactJS',
+          porcentage: '80%',
+          value: '80'
         },
         {
-          id: "Python_skill",
-          content: "Python",
-          porcentage: "75%",
-          value: "75"
+          id: 'JavaScript_skill',
+          content: 'JavaScript',
+          porcentage: '80%',
+          value: '80'
         },
         {
-          id: "VanillaJS_skill",
-          content: "VanillaJS",
-          porcentage: "85%",
-          value: "85"
+          id: 'TypeScript_skill',
+          content: 'TypeScript',
+          porcentage: '80%',
+          value: '80'
         },
         {
-          id: "Wordpress_skill",
-          content: "Wordpress",
-          porcentage: "80%",
-          value: "80"
-        }
+          id: 'Redux_skill',
+          content: 'Redux',
+          porcentage: '80%',
+          value: '80'
+        },
+        {
+          id: 'JavaSE_skill',
+          content: 'Java SE',
+          porcentage: '80%',
+          value: '80'
+        },
+        {
+          id: 'JavaEE_skill',
+          content: 'Java EE',
+          porcentage: '60%',
+          value: '60'
+        },
+        {
+          id: 'SpringBoot_skill',
+          content: 'Spring Boot',
+          porcentage: '40%',
+          value: '40'
+        },
+        {
+          id: 'OOP_skill',
+          content: 'OOP',
+          porcentage: '80%',
+          value: '80'
+        },
+        {
+          id: 'Firebase_skill',
+          content: 'Firebase',
+          porcentage: '90%',
+          value: '90'
+        },
+        {
+          id: 'MongoDB_skill',
+          content: 'MongoDB',
+          porcentage: '50%',
+          value: '50'
+        },
+        {
+          id: 'Selenium_skill',
+          content: 'Selenium',
+          porcentage: '60%',
+          value: '60'
+        },
+        { id: 'HTML5_skill', content: 'HTML5', porcentage: '80%', value: '80' },
+        { id: 'CSS3_skill', content: 'CSS3', porcentage: '70%', value: '70' }
       ],
       about_me: [
         {
-          id: "first-p-about",
-          content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+          id: 'first-p-about',
+          content: 'I am an enthusiastic and self-motivated software engineer'
         },
         {
-          id: "second-p-about",
+          id: 'second-p-about',
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            'Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis.'
         },
         {
-          id: "third-p-about",
+          id: 'third-p-about',
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            'Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis.'
         }
       ]
     };
@@ -72,11 +106,11 @@ class About extends React.Component {
                     <div className="row">
                       <div
                         className="col-sm-6 col-md-5"
-                        style={{ margin: "0 auto" }}
+                        style={{ margin: '0 auto' }}
                       >
                         <div
                           className="about-img"
-                          style={{ textAlign: "center" }}
+                          style={{ textAlign: 'center' }}
                         >
                           <img
                             className="img-fluid rounded b-shadow-a"
@@ -90,7 +124,7 @@ class About extends React.Component {
                       {this.state.skills.map(skill => {
                         return (
                           <React.Fragment key={skill.id}>
-                            <span>{skill.content}</span>{" "}
+                            <span>{skill.content}</span>{' '}
                             <span className="pull-right">
                               {skill.porcentage}
                             </span>
