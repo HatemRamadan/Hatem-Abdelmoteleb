f997593dc5226acd094d497dd67d7a469a0f7d33
HatemRamadan <hatemram96@gmail.com>
2025-02-20 02:03:35 +0200

    Revert "push build"
    This reverts commit 4b979196d3f8c415f3544611399001977b7ac7f8.

--- src/config.firebase.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyABfT-Ubv5spwWfByLXny6szvsKB54hG98',
-  authDomain: 'hatem-portfolio.firebaseapp.com',
-  projectId: 'hatem-portfolio',
-  storageBucket: 'hatem-portfolio.appspot.com',
-  messagingSenderId: '981672120778',
-  appId: '1:981672120778:web:c09cc3b40ae70c23f93f33',
-  measurementId: 'G-MJ7D9565W2'
-};
